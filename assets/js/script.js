3f76d9e13a999270a458e6990f109a7ec2c4ddd2
Jammie506 <mccarthyjamie506@gmail.com>
2024-04-27 17:52:35 +0100

    Add base JS for Timer Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,40 @@
+//Global Variables
+
+//For examTimer & endAlert()
+var secondsBetweenActions = 5;
+var secondsRemaining = secondsBetweenActions;
+var examInterval;
+
 function examStart() {
     // Get Checkbox
     var checkBox = document.getElementById("exam-start");
-    // Get the output text
+    // Get Exam Content
     var text = document.getElementById("exam-content");
-  
-    // If the checkbox is checked, display the output text
+
+    examInterval = setInterval(myTimer, 1000);
+
+    // Hide Exam Content Until Exam Starts
     if (checkBox.checked == true){
       text.style.display = "block";
+      //Start Timer when Exam Starts
+      examInterval;
+      console.log("Timer Start");
     } else {
       text.style.display = "none";
     }
+
+  }
+
+  function myTimer(){
+    secondsRemaining --;
+    console.log(secondsRemaining);
+    if(secondsRemaining == 0){
+        endAlert();
+    }
+  }
+
+  function endAlert(){
+    alert("Time Up! Exam will automatically submit");
+    clearInterval(examInterval);
+    console.log("Timer Cleared");
   }
