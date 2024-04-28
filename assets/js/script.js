576b62d4f1e3ccf5c09723e119e5a021c28a2c77
Jammie506 <mccarthyjamie506@gmail.com>
2024-04-28 13:36:47 +0100

    Add Psuedo-Randomization to Exam Questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,60 +26,71 @@ let submitted = document.getElementById("exam-submitted");
 let timer = document.getElementById("timer");
 
 function examStart() {
-    //Establishing Timer
-    examInterval = setInterval(myTimer, 1000);
+  //Establishing Timer
+  examInterval = setInterval(myTimer, 1000);
 
-    // Hide Exam Content Until Exam Starts
-    if (checkBox.checked == true){
-        //Hide Exam Intro and Show Exam
-        //TODO Set up Question Randomization 
-        exam.style.display = "block";
-        intro.style.display = "none"
+  // Hide Exam Content Until Exam Starts
+  if (checkBox.checked == true){
+    //Hide Exam Intro and Show Exam
+    //TODO Set up Question Randomization 
 
-        //Start Timer when Exam Starts
-        examInterval;
-        console.log("Timer Start");
-    } else {
-        exam.style.display = "none";
-    }
+    exam.style.display = "block";
+    intro.style.display = "none";
 
+  for(let i = 0; i < 2; i++){
+    let j = Math.floor(Math.random()*shortQuestions.length);
+    shortQuestions[j].style.display = "none";
   }
 
-  function myTimer(){
-    secondsRemaining --;
-    console.log(secondsRemaining);
-    let timeRemaining;
-
-    // Converting Seconds into Minutes and Seconds for User Timer
-    let min = Math.floor (secondsRemaining / 60);
-    let sec = secondsRemaining % 60;
-
-    // Force Min & Seconds to be Double Digits
-    let minFull = String(min.toString()).padStart(2, '0');
-    let secFull = String(sec.toString()).padStart(2, '0');
-
-    timeRemaining = minFull + ":" +secFull;
-
-    timer.innerHTML = timeRemaining;
-    if(secondsRemaining == 0){
-        endAlert();
-    }
+  for(let l = 0; l < 3; l++){
+    let k = Math.floor(Math.random()*longQuestions.length);
+    longQuestions[k].style.display = "none";
   }
 
-  function endAlert(){
-    alert("Time Up! Exam will automatically submit");
-    submitExam();
-  }
-
-  //TODO Set up Automatic Validation for Preliminary Grade
-  function submitExam(){
-    console.log("Submission in Progress")
-    // End Exam Timer Early
-    clearInterval(examInterval);
-    console.log("Timer Cleared")
-
+  //Start Timer when Exam Starts
+  examInterval;
+  console.log("Timer Start");
+} else {
     exam.style.display = "none";
-    submitted.style.display = "block";
+}
 
-    console.log("Exam Submitted");
+}
+
+function myTimer(){
+  secondsRemaining --;
+  console.log(secondsRemaining);
+  let timeRemaining;
+
+  // Converting Seconds into Minutes and Seconds for User Timer
+  let min = Math.floor (secondsRemaining / 60);
+  let sec = secondsRemaining % 60;
+
+  // Force Min & Seconds to be Double Digits
+  let minFull = String(min.toString()).padStart(2, '0');
+  let secFull = String(sec.toString()).padStart(2, '0');
+
+  timeRemaining = minFull + ":" +secFull;
+
+  timer.innerHTML = timeRemaining;
+  if(secondsRemaining == 0){
+      endAlert();
   }
+}
+
+function endAlert(){
+  alert("Time Up! Exam will automatically submit");
+  submitExam();
+}
+
+//TODO Set up Automatic Validation for Preliminary Grade
+function submitExam(){
+  console.log("Submission in Progress")
+  // End Exam Timer Early
+  clearInterval(examInterval);
+  console.log("Timer Cleared")
+
+  exam.style.display = "none";
+  submitted.style.display = "block";
+
+  console.log("Exam Submitted");
+}
